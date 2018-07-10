568e6f9ed520a96bbf64559dac8417fffb57c7aa
徳地澄人 <sumito@tokudjisumito-no-MacBook-Air.local>
2018-07-10 12:42:29 -0500

    Misc.

--- hugs/plots/special_plot.py
+++ hugs/plots/special_plot.py
@@ -1,4 +1,21 @@
-"""Makes special plots for our library."""
+"""Make a random plot of some size for science.
+
+Makes a random plot of num_points and colored randomly. While this probably
+sounds silly, it sometimes shows more patterns that some real dataself.
+
+Parameters
+----------
+num_points : integer
+    The number of random points to plot on the graph.
+seed : integer, optional
+    Seed value for the random number generator for reproducible plots.
+
+Returns
+-------
+matplotlib.figure.Figure : figure object
+matplotlib.axes._subplots.AxesSubplot : axes
+
+"""
 
 import matplotlib.pyplot as plt
 
